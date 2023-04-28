a21bc69592999a26aeeb2f4d8548541c68d226e9
IceFire246 <39646256+IceFire246@users.noreply.github.com>
2023-04-28 21:16:44 +0800

    Practical progress

--- app/src/main/java/com/example/week2/MainActivity.java
+++ app/src/main/java/com/example/week2/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.week2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btnFollow);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (User.followed){
+            User.followed = false;
+            btn.setText("Follow");
+        }
+        else {
+            User.followed = true;
+            btn.setText("Unfollow");
+        }
     }
 }
--- app/src/main/java/com/example/week2/User.java
+++ app/src/main/java/com/example/week2/User.java
@@ -1,41 +1,10 @@
 package com.example.week2;
 
 public class User {
-    private String name;
-    private String description;
-    private int id;
-    private boolean followed;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String newName) {
-        this.name = newName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String newDesc) {
-        this.description = newDesc;
-    }
-
-    public int getId() {
-        return description;
-    }
-
-    public void setId(int newId) {
-        this.id = newId
-    }
-
-    public boolean getFollowed() {
-        return followed;
-    }
-
-    public void setFollowed(boolean newFollow) {
-        this.followed = newFollow
-    }
+    public String name;
+    public String description;
+    public int id;
+    public static boolean followed;
 }
-}
+
+
